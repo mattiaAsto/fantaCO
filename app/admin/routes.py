297beb9bcdbe3cf10c2a00aa879b878b94ed31d9
Mattia <134499729+mattiaAsto@users.noreply.github.com>
2025-02-18 16:05:32 +0000

    bugfix

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -18,13 +18,13 @@ class AdminOnlyView(ModelView):
     def on_model_change(self, form, model, is_created):
         # Quando l'utente seleziona il checkbox, vogliamo che il valore sia la stringa 'True'
         if form.is_validated.data:  # Se il checkbox è selezionato
-            model.is_validated = 'True'  # Impostiamo la stringa 'True'
+            model.is_validated = True  # Impostiamo la stringa 'True'
         else:  # Se il checkbox non è selezionato
-            model.is_validated = 'False'  # Impostiamo la stringa 'False'
+            model.is_validated = False  # Impostiamo la stringa 'False'
 
         if form.light_theme.data:  # Per il checkbox light_theme
-            model.light_theme = 'True'  # Impostiamo la stringa 'True'
+            model.light_theme = True  # Impostiamo la stringa 'True'
         else:
-            model.light_theme = 'False'  # Impostiamo la stringa 'False'
+            model.light_theme = False  # Impostiamo la stringa 'False'
 
         return super(AdminOnlyView, self).on_model_change(form, model, is_created)
