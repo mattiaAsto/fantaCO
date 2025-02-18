acb3af9b08fbd84f0625c3d5543b6f10a69f35f8
Mattia <134499729+mattiaAsto@users.noreply.github.com>
2025-02-18 15:58:08 +0000

    bugfix

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,7 +1,7 @@
 from flask_admin.contrib.sqla import ModelView
-from flask import redirect, url_for
-from flask_login import current_user
 from wtforms import BooleanField
+from flask_login import current_user
+from flask import redirect, url_for
 
 class AdminOnlyView(ModelView):
     form_overrides = {
@@ -9,23 +9,22 @@ class AdminOnlyView(ModelView):
         'light_theme': BooleanField
     }
 
-    def on_model_change(self, form, model, is_created):
-        # Converte i valori 'y' e 'n' in True e False
-        if form.is_validated.data == 'y':
-            model.is_validated = True
-        elif form.is_validated.data == 'n':
-            model.is_validated = False
-
-        if form.light_theme.data == 'y':
-            model.light_theme = True
-        elif form.light_theme.data == 'n':
-            model.light_theme = False
-
-        # Chiamare il metodo della classe base per assicurarsi che il cambiamento venga applicato
-        return super(AdminOnlyView, self).on_model_change(form, model, is_created)
     def is_accessible(self):
         return current_user.is_authenticated and current_user.username == "admin"
 
     def inaccessible_callback(self, name, **kwargs):
-        """Se l'utente non è admin, viene reindirizzato alla pagina di login"""
         return redirect(url_for('auth.login'))
+
+    def on_model_change(self, form, model, is_created):
+        # Quando l'utente seleziona il checkbox, vogliamo che il valore sia la stringa 'True'
+        if form.is_validated.data:  # Se il checkbox è selezionato
+            model.is_validated = 'True'  # Impostiamo la stringa 'True'
+        else:  # Se il checkbox non è selezionato
+            model.is_validated = 'False'  # Impostiamo la stringa 'False'
+
+        if form.light_theme.data:  # Per il checkbox light_theme
+            model.light_theme = 'True'  # Impostiamo la stringa 'True'
+        else:
+            model.light_theme = 'False'  # Impostiamo la stringa 'False'
+
+        return super(AdminOnlyView, self).on_model_change(form, model, is_created)
