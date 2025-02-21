1653073cc99fee3c7c514475438866b7eb4b31b7
mattiaAstori <astorimattia05gmail.com>
2025-02-21 12:13:39 +0100

    migrate=True

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ with open(league_path, 'r') as file:
 with open(POINTS_PATH, 'r') as file:
     points_table = json.load(file)
 
-migrate=False
+migrate=True
 if migrate:
     with app.app_context():
 
